5d9c664badae75888f84d3b7efd4856b6961536a
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-09 16:57:37 +0200

    Lake navigation now dynamically rendered

--- lakes/views.py
+++ lakes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lakes import models
+import itertools
 
 # Create your views here.
 def index(request):
-    context = {}
+    lakes = models.Lake.objects.select_related('district')
+    grouped = itertools.groupby(lakes, key=lambda x: x.district.name)
+    lakesByDistrict = [(k, [{'name': l.name, 'slug': l.slug()} for l in lakes]) for k,lakes in grouped]
+    
+    context = { 'lakesByDistrict': lakesByDistrict }
     return render(request, 'lakes/index.html', context)
