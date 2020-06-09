ce715726153b243f0f4d743c1ec37c41e89d6baf
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-09 08:42:15 +0200

    Add first models

--- lakes/models.py
+++ lakes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.ForeignKey(
+        State, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+class Lake(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(
+        District, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+class DiveSpot(models.Model):
+    name = models.CharField(max_length=100)
+    lake = models.ForeignKey(
+        Lake, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
