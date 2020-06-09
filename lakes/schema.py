7306e95661474c7f1c2d13abdcad900743d6e76d
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-09 11:21:25 +0200

    Add district.lakes resolver

--- lakes/schema.py
+++ lakes/schema.py
@@ -9,20 +9,32 @@ class CountryType(DjangoObjectType):
 class StateType(DjangoObjectType):
     class Meta:
         model = models.State
-class DistrictType(DjangoObjectType):
-    class Meta:
-        model = models.District
 class LakeType(DjangoObjectType):
     class Meta:
         model = models.Lake
+class DistrictType(DjangoObjectType):
+    class Meta:
+        model = models.District
+    lakes = graphene.List(LakeType)
+    def resolve_lakes(self, info):
+        return models.Lake.objects.filter(district_id=self.id)
 class DivespotType(DjangoObjectType):
     class Meta:
         model = models.Divespot
 
 class Query(object):
+    countries = graphene.List(CountryType)
+    states = graphene.List(StateType)
+    districts = graphene.List(DistrictType)
     lakes = graphene.List(LakeType)
     divespots = graphene.List(DivespotType)
 
+    def resolve_countries(self, info, **kwargs):
+        return models.Country.objects.all()
+    def resolve_states(self, info, **kwargs):
+        return models.State.objects.all()
+    def resolve_districts(self, info, **kwargs):
+        return models.District.objects.all()
     def resolve_lakes(self, info, **kwargs):
         return models.Lake.objects.all()
     def resolve_divespots(self, info, **kwargs):
