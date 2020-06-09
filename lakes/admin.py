afdc1febde13c99f9496c9e38ec26439c003e5e9
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-09 09:29:12 +0200

    Register models

--- lakes/admin.py
+++ lakes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from lakes import models
 
 # Register your models here.
+admin.site.register(models.Country)
+admin.site.register(models.State)
+admin.site.register(models.District)
+admin.site.register(models.Lake)
+admin.site.register(models.Divespot)
