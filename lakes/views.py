69ab3a1e9d1883f147fa92ac40dae0bd81ff8480
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-10 10:04:37 +0200

    Show divemap in page

--- lakes/views.py
+++ lakes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from lakes import models, queries
+import itertools
 
 # Create your views here.
 def index(request):
@@ -13,11 +14,18 @@ def lake(request, lakeSlug):
     lakesByDistrict = queries.getLakesByDistrict()
     lake = models.Lake.objects.get(slug=lakeSlug)
     divespots = models.Divespot.objects.filter(lake=lake)
+    divemapsByDivespot = [(key, [m for m in maps]) \
+        for key, maps in itertools.groupby(
+        models.Divemap.objects.filter(divespot__in=divespots),
+        key=lambda m: m.divespot_id,
+    )]
 
     context = { 
         'lakesByDistrict': lakesByDistrict, 
         'lake': lake, 
         'divespots': divespots,
         'divespotCount': divespots.count(),
+        'divemapsByDivespot': divemapsByDivespot,
     }
+
     return render(request, 'lakes/lake.html', context)
