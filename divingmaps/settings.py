90ab1487739ad8624e9201d5b4030a80e27284d6
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-10 14:29:04 +0200

    Use mysql

--- divingmaps/settings.py
+++ divingmaps/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env()
+env.read_env('./.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'divingmaps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'divemaps',
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'default-character-set': 'utf8',
+        'sql_mode': 'STRICT_TRANS_TABLES',
     }
 }
 
