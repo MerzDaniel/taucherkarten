4c9a316cc5009d75b626c9f9dfdfdb4168ee983a
Daniel Merz <MerzDanielDeveloper@gmail.com>
2020-06-10 09:20:13 +0200

    Serve uploaded images

--- divingmaps/urls.py
+++ divingmaps/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', GraphQLView.as_view(graphiql=True)),
     path('', include('lakes.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
